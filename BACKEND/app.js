0880691d424ddf48fbaf5ccd803059466fec512e
paulto <tovanhuong007@gmail.com>
2024-11-07 00:45:55 +0700

    fix some problems

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const chitietphieuxuatRoutes = require('./routes/chitietphieuxuatRoutes');
+const nhanvienRoutes = require('./routes/nhanvienRoutes');
 
 const app = express();
+// Use CORS middleware
+app.use(cors());
+
 app.use(bodyParser.json());
 
 app.use('/api/chitietphieuxuat', chitietphieuxuatRoutes);
+app.use('/api/nhanvien', nhanvienRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
---
+++ BACKEND/models/nhanvienModel.js
@@ -0,0 +1,10 @@
+const db = require('../config/db');
+
+const NhanVien = {
+    create: (data, callback) => {
+        const sql = 'INSERT INTO NhanVien (HoTen, NgaySinh, SoDienThoai) VALUES (?, ?, ?)';
+        db.query(sql, [data.HoTen, data.NgaySinh, data.SoDienThoai], callback);
+    }
+};
+
+module.exports = NhanVien;
